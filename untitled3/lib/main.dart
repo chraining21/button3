ee406e5a94d1c29bc695938857f79cecd75cf76a
chraining21 <74174198+chraining21@users.noreply.github.com>
2022-03-30 14:57:57 +0800

    20220329

---
+++ untitled3/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'app_body2.dart';
+
+void main() => runApp(App());
+
+class App extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    var appTitle = Text('Flutter Button');
+
+    var appBody = AppBody();
+
+    var appBar = AppBar(
+      title:appTitle,
+    );
+
+    var app = MaterialApp(
+        home : Scaffold(
+          appBar: appBar,
+          body: appBody,
+        )
+    );
+    return app;
+  }
+}
